f8545b860ab4a38f20771dea3c5c7c97c92d00a1
Thuy Luu <thuyluu@magenest.com>
2023-11-10 10:40:07 +0700

    init

---
+++ extensions/fashion-size-chart/assets/fashion-sizechart.js
@@ -0,0 +1,6 @@
+console.log(window.Shopify.shop);
+var pElement = document.getElementById('sizechartproductid');
+
+// To get the value of 'theId' attribute
+var theIdValue = pElement.getAttribute('data-theProductId');
+console.log(theIdValue);
